0fe3d38bfc54a9db45ab1ff904db717260bc26aa
everyoneselectronic <fogertyliam@gmail.com>
2016-10-08 14:17:21 +0100

    as
    asd

--- routes/sinch.js
+++ routes/sinch.js
@@ -21,17 +21,17 @@ var svamlResponse =
 			}
 		],
 		action: {
-			// "name": "ConnectConf",
-			// "conferenceId": "myconference1",
-			// "cli": "",
-			// "suppressCallbacks": true
+			"name": "ConnectConf",
+			"conferenceId": "myconference1",
+			"cli": "",
+			"suppressCallbacks": true
 		}
 	}
 
 router.post('/', function (req, res, next) {
 	//we know its a ICE event since we supress callbacks for other events
 	// set the callerid to the calling number
-	// svamlResponse.action.cli = req.body.cli;
+	svamlResponse.action.cli = req.body.cli;
 	//send back the response.
 	res.json(svamlResponse);
 });
